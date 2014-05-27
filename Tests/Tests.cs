2c6111101bfb8dff91793a253c5462d27186921f
Rob Rodi <robert.rodi@gmail.com>
2014-05-27 11:32:05 -0700

    first, dumb test. added HttpClient

---
+++ Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace CachedHttpClient.Tests
+{
+    using System.Net.Http;
+    using System.Runtime.Remoting.Channels;
+
+    using Xunit;
+
+    public class CachedHttpClient
+    {
+        private readonly HttpClient client = new HttpClient();
+    }
+
+    [Trait("Basic Http Client", "")]
+    public class Tests
+    {
+        [Fact(DisplayName = "Connects to an external Serivce")]
+        public void Connect()
+        {
+            var client = new CachedHttpClient();
+
+            
+        }
+    }
+}
