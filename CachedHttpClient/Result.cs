1c4a94dad4a0fcdbb85700da9de979d27e1f8b5a
Rob Rodi <robert.rodi@gmail.com>
2014-06-02 21:33:36 -0700

    Move Result to

---
+++ CachedHttpClient/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace CachedHttpClient
+{
+    using System.Net;
+
+    public class Result
+    {
+        public Result(string body, HttpStatusCode status, bool wasCached)
+        {
+            this.Body = body;
+            this.StatusCode = status;
+            this.WasCached = wasCached;
+        }
+
+        public string Body { get; private set; }
+        public HttpStatusCode StatusCode { get; private set; }
+        public bool WasCached { get; private set; }
+    }
+}
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -18,19 +18,6 @@
 
     using Xunit;
 
-    public class Result
-    {
-        public Result(string body, HttpStatusCode status, bool wasCached)
-        {
-            this.Body = body;
-            this.StatusCode = status;
-            this.WasCached = wasCached;
-        }
-
-        public string Body { get; private set; }
-        public HttpStatusCode StatusCode { get; private set; }
-        public bool WasCached { get; private set; }
-    }
     public class CachedHttpClient
     {
         private static readonly ObjectCache _cache;
