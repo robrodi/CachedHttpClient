aae7de5835a8ad6e3620066fd7f2ee734b4a98dc
Rob Rodi <robert.rodi@gmail.com>
2014-06-02 22:12:27 -0700

    Comments Hide test stuff.

--- CachedHttpClient/CachedHttpClient.cs
+++ CachedHttpClient/CachedHttpClient.cs
@@ -21,22 +21,40 @@
         /// </summary>
         private readonly ObjectCache _instanceCache;
 
+        /// <summary>
+        /// Initializes the <see cref="CachedHttpClient"/> class.
+        /// </summary>
         static CachedHttpClient()
         {
             _cache = new MemoryCache("CachedHttp");
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CachedHttpClient"/> class.
+        /// </summary>
         public CachedHttpClient()
             : this(new HttpClient())
         {
         }
 
-        public CachedHttpClient(HttpClient client, ObjectCache objectCache = null)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CachedHttpClient"/> class.
+        /// Note: this is primaraliy used for testing.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        /// <param name="objectCache">The object cache.</param>
+        internal CachedHttpClient(HttpClient client, ObjectCache objectCache = null)
         {
             this.client = client;
             this._instanceCache = objectCache ?? _cache;
         }
 
+        /// <summary>
+        /// Gets the cache. First uses the instance cache, if available, then falls over to static.
+        /// </summary>
+        /// <value>
+        /// The cache.
+        /// </value>
         private ObjectCache cache
         {
             get
@@ -45,7 +63,13 @@
             }
         }
 
-        public static ObjectCache Cache
+        /// <summary>
+        /// Gets the cache.
+        /// </summary>
+        /// <value>
+        /// The cache.
+        /// </value>
+        internal static ObjectCache Cache
         {
             get
             {
@@ -95,53 +119,104 @@
             return new Result(result.Content.ToString(), result.StatusCode, false);
         }
 
+        /// <summary>
+        /// Send a POST request to the specified Uri as an asynchronous operation.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Returns <see cref="T:System.Threading.Tasks.Task`1"/>.The task object representing the asynchronous operation.
+        /// </returns>
+        /// <param name="requestUri">The Uri the request is sent to.</param><param name="content">The HTTP request content sent to the server.</param><exception cref="T:System.ArgumentNullException">The <paramref name="requestUri"/> was null.</exception>
         public Task<HttpResponseMessage> PostAsync(Uri requestUri, HttpContent content)
         {
             return this.PostAsync(requestUri, content, CancellationToken.None);
         }
 
+        /// <summary>
+        /// Send a POST request with a cancellation token as an asynchronous operation.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Returns <see cref="T:System.Threading.Tasks.Task`1"/>.The task object representing the asynchronous operation.
+        /// </returns>
+        /// <param name="requestUri">The Uri the request is sent to.</param><param name="content">The HTTP request content sent to the server.</param><param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param><exception cref="T:System.ArgumentNullException">The <paramref name="requestUri"/> was null.</exception>
         public async Task<HttpResponseMessage> PostAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken)
         {
             var result = await client.PostAsync(requestUri, content, cancellationToken);
-            this.ClearCacheKeyIfSuccessful(requestUri, result);
+            ClearCacheKeyIfSuccessful(requestUri, result, this.cache);
             return result;
         }
 
-        private void ClearCacheKeyIfSuccessful(Uri requestUri, HttpResponseMessage result)
-        {
-            if (result.IsSuccessStatusCode)
-            {
-                this.cache.Remove(requestUri.ToString());
-            }
-        }
-
-        public Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content)
+        /// <summary>
+        /// Send a PUT request to the specified Uri as an asynchronous operation.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Returns <see cref="T:System.Threading.Tasks.Task`1"/>.The task object representing the asynchronous operation.
+        /// </returns>
+        /// <param name="requestUri">The Uri the request is sent to.</param><param name="content">The HTTP request content sent to the server.</param><exception cref="T:System.ArgumentNullException">The <paramref name="requestUri"/> was null.</exception>
+       public Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content)
         {
             return this.PutAsync(requestUri, content, CancellationToken.None);
         }
 
+       /// <summary>
+       /// Send a PUT request with a cancellation token as an asynchronous operation.
+       /// </summary>
+       /// 
+       /// <returns>
+       /// Returns <see cref="T:System.Threading.Tasks.Task`1"/>.The task object representing the asynchronous operation.
+       /// </returns>
+       /// <param name="requestUri">The Uri the request is sent to.</param><param name="content">The HTTP request content sent to the server.</param><param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param><exception cref="T:System.ArgumentNullException">The <paramref name="requestUri"/> was null.</exception>
         public async Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken)
         {
             var result = await client.PostAsync(requestUri, content, cancellationToken);
-            this.ClearCacheKeyIfSuccessful(requestUri, result);
+            ClearCacheKeyIfSuccessful(requestUri, result, this.cache);
             return result;
         }
 
+        /// <summary>
+        /// Send a DELETE request to the specified Uri as an asynchronous operation.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Returns <see cref="T:System.Threading.Tasks.Task`1"/>.The task object representing the asynchronous operation.
+        /// </returns>
+        /// <param name="requestUri">The Uri the request is sent to.</param><exception cref="T:System.ArgumentNullException">The <paramref name="requestUri"/> was null.</exception><exception cref="T:System.InvalidOperationException">The request message was already sent by the <see cref="T:System.Net.Http.HttpClient"/> instance.</exception>
         public Task<HttpResponseMessage> DeleteAsync(Uri requestUri)
         {
             return this.DeleteAsync(requestUri, CancellationToken.None);
         }
 
+        /// <summary>
+        /// Send a DELETE request to the specified Uri with a cancellation token as an asynchronous operation.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Returns <see cref="T:System.Threading.Tasks.Task`1"/>.The task object representing the asynchronous operation.
+        /// </returns>
+        /// <param name="requestUri">The Uri the request is sent to.</param><param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param><exception cref="T:System.ArgumentNullException">The <paramref name="requestUri"/> was null.</exception><exception cref="T:System.InvalidOperationException">The request message was already sent by the <see cref="T:System.Net.Http.HttpClient"/> instance.</exception>
         public async Task<HttpResponseMessage> DeleteAsync(Uri requestUri, CancellationToken cancellationToken)
         {
             var result = await client.DeleteAsync(requestUri, cancellationToken);
-            this.ClearCacheKeyIfSuccessful(requestUri, result);
+            ClearCacheKeyIfSuccessful(requestUri, result, this.cache);
             return result;
         }
 
+        /// <summary>
+        /// Cancel all pending requests on this instance.
+        /// </summary>
         public void CancelPendingRequests()
         {
             client.CancelPendingRequests();
+        }
+
+        private static void ClearCacheKeyIfSuccessful(Uri requestUri, HttpResponseMessage result, ObjectCache cache)
+        {
+            if (result.IsSuccessStatusCode)
+            {
+                cache.Remove(requestUri.ToString());
+            }
         }
     }
 }
